1299e6fb7f87bd201e9b39690b919907221a761a
alpersmb <alper1ergin@gmail.com>
2024-04-08 00:54:12 +0300

    recreate splash and main activities

---
+++ app/src/main/java/com/example/kultursanat/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.kultursanat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
