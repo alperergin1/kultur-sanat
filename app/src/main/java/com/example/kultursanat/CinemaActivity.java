6174faae86b5f2fcb4dd5c1c28d4a9c7d1d3f23d
alpersmb <alper1ergin@gmail.com>
2024-04-09 01:35:26 +0300

    navigate to movie seats from cinema and rest of them fixed mixed etc

--- app/src/main/java/com/example/kultursanat/CinemaActivity.java
+++ app/src/main/java/com/example/kultursanat/CinemaActivity.java
@@ -1,7 +1,10 @@
 package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CinemaActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class CinemaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cinema);
 
-
+        findViewById(R.id.button_film_saatlerine_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieTimesActivity();
+            }
+        });
+    }
+    private void startMovieTimesActivity() {
+        Intent intent = new Intent(CinemaActivity.this, MovieTimesActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kultursanat/MovieTimesActivity.java
+++ app/src/main/java/com/example/kultursanat/MovieTimesActivity.java
@@ -2,7 +2,9 @@ package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MovieTimesActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MovieTimesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_times);
+
+        findViewById(R.id.button_koltuk_secmeye_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
+    }
+    private void startMovieSeatsActivity() {
+        Intent intent = new Intent(MovieTimesActivity.this, MovieSeatsActivity.class);
+        startActivity(intent);
     }
 }
