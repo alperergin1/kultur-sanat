81694f3eb743b09a4aec5120a580b5b7f5d17b3f
alpersmb <alper1ergin@gmail.com>
2024-04-08 22:27:06 +0300

    Theaters activity created for theatre plays

---
+++ app/src/main/java/com/example/kultursanat/TheatersActivity.java
@@ -0,0 +1,14 @@
+package com.example.kultursanat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TheatersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theaters);
+    }
+}
