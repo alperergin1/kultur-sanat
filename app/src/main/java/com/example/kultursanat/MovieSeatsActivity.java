a55085f922e72bfea13ea1ed86046a9a93d7977a
alpersmb <alper1ergin@gmail.com>
2024-04-09 01:44:53 +0300

    navigate to Summ and Confir.. screen from MovieSeatd

--- app/src/main/java/com/example/kultursanat/MovieSeatsActivity.java
+++ app/src/main/java/com/example/kultursanat/MovieSeatsActivity.java
@@ -2,7 +2,9 @@ package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MovieSeatsActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MovieSeatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_seats);
+        findViewById(R.id.button_film_koltuk_onay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSummaryAndConfirmationActivity();
+            }
+        });
+    }
+
+    private void startSummaryAndConfirmationActivity() {
+        Intent intent = new Intent(MovieSeatsActivity.this, SummaryAndConfirmationActivity.class);
+        startActivity(intent);
     }
 }
