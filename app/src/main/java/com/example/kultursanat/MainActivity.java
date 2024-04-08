9a2575dcad7695626ac64df47c8dc7a5cd58bc18
alpersmb <alper1ergin@gmail.com>
2024-04-09 01:11:12 +0300

    navigate to cinema screen from main

--- app/src/main/java/com/example/kultursanat/MainActivity.java
+++ app/src/main/java/com/example/kultursanat/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_sinemaya_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCinemaActivity();
+            }
+        });
+    }
+
+    private void startCinemaActivity() {
+        Intent intent = new Intent(MainActivity.this, CinemaActivity.class);
+        startActivity(intent);
     }
 }
