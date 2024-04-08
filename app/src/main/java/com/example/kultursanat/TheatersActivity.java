bf0bc19ad892bb1b0f16278fb91df578fa6e9300
alpersmb <alper1ergin@gmail.com>
2024-04-09 02:13:20 +0300

    navigate to theatre seats from theatres sth...

--- app/src/main/java/com/example/kultursanat/TheatersActivity.java
+++ app/src/main/java/com/example/kultursanat/TheatersActivity.java
@@ -2,7 +2,9 @@ package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TheatersActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class TheatersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theaters);
+        findViewById(R.id.button_tiyatro_koltuk_secmeye_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTheatreSeatsActivity();
+            }
+        });
+    }
+
+    private void startTheatreSeatsActivity() {
+        Intent intent = new Intent(TheatersActivity.this, TheatreSeatsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kultursanat/TheatreSeatsActivity.java
+++ app/src/main/java/com/example/kultursanat/TheatreSeatsActivity.java
@@ -2,7 +2,9 @@ package com.example.kultursanat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TheatreSeatsActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class TheatreSeatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theatre_seats);
+        findViewById(R.id.button_ozet_onaya_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSummaryAndConfirmationActivity();
+            }
+        });
+    }
+
+    private void startSummaryAndConfirmationActivity() {
+        Intent intent = new Intent(TheatreSeatsActivity.this, SummaryAndConfirmationActivity.class);
+        startActivity(intent);
     }
 }
