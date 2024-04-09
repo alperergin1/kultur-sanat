8c46ba4e1fc27d8dfd0b3195da34567a71e0d267
alpersmb <alper1ergin@gmail.com>
2024-04-09 20:52:16 +0300

    updated login screen username&password

--- app/src/main/java/com/example/kultursanat/LoginActivity.java
+++ app/src/main/java/com/example/kultursanat/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,10 +20,32 @@ public class LoginActivity extends AppCompatActivity {
                 startMainActivity();
             }
         });
+
+        findViewById(R.id.button_login_yap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_yap();
+            }
+        });
+
     }
 
     private void startMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
+
+    private void login_yap() {
+
+        EditText editText_username = findViewById(R.id.editText_username);
+        EditText editText_password = findViewById(R.id.editText_password);
+        String kullaniciAdi = editText_username.getText().toString();
+        String sifre = editText_password.getText().toString();
+
+        TextView textView_username = findViewById(R.id.textView_username);
+        TextView textView_password = findViewById(R.id.textView_password);
+        textView_username.setText("Kullanıcı Adı: " + kullaniciAdi);
+        textView_password.setText("Şifre: " + sifre);
+    }
+
 }
