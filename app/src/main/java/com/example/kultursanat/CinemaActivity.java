79522f63ec713e11940396ba308db14a411cb223
alpersmb <alper1ergin@gmail.com>
2024-04-11 02:48:43 +0300

    ufak düzeltmeler

--- app/src/main/java/com/example/kultursanat/CinemaActivity.java
+++ app/src/main/java/com/example/kultursanat/CinemaActivity.java
@@ -13,15 +13,11 @@ public class CinemaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cinema);
 
-        findViewById(R.id.button_film_saatlerine_git).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startMovieTimesActivity();
-            }
-        });
+
     }
-    private void startMovieTimesActivity() {
+    /*private void startMovieTimesActivity() {
         Intent intent = new Intent(CinemaActivity.this, MovieTimesActivity.class);
         startActivity(intent);
     }
+     */
 }
