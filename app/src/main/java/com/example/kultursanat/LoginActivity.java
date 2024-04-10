eead5c9e6b3e8632084f719bf507c020ec6c6a12
alpersmb <alper1ergin@gmail.com>
2024-04-10 17:34:40 +0300

    signup butonu eklendi, kayıt olma eklendi

--- app/src/main/java/com/example/kultursanat/LoginActivity.java
+++ app/src/main/java/com/example/kultursanat/LoginActivity.java
@@ -41,8 +41,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-
+        findViewById(R.id.button_signup_yap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup_yap();
+            }
+        });
     }
 
     private void startMainActivity() {
@@ -52,6 +56,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login_yap() {
 
+    }
+
+    private void signup_yap() {
+        EditText editText_username = findViewById(R.id.editText_username);
+        EditText editText_password = findViewById(R.id.editText_password);
+        String kullaniciAdiSignup = editText_username.getText().toString();
+        String sifreSignup = editText_password.getText().toString();
+
+        TextView textView_signup_username = findViewById(R.id.textView_username);
+        TextView textView_signup_password = findViewById(R.id.textView_password);
+
+        textView_signup_username.setText(getString(R.string.username_signup_textview) + " " + kullaniciAdiSignup);
+        textView_signup_password.setText(getString(R.string.password_signup_textview) + " " + sifreSignup);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", kullaniciAdiSignup);
+        editor.putString("password", sifreSignup);
+        editor.apply();
     }
 
 }
