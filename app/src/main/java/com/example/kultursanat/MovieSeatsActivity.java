87569e0c7e3bd3a1212fa5ba713c329462f4e6d1
alpersmb <alper1ergin@gmail.com>
2024-04-11 05:29:55 +0300

    koltuk seçildi ve kaydedildi

--- app/src/main/java/com/example/kultursanat/MovieSeatsActivity.java
+++ app/src/main/java/com/example/kultursanat/MovieSeatsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MovieSeatsActivity extends AppCompatActivity {
@@ -14,10 +15,19 @@ public class MovieSeatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_seats);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        String selectedSeans = sharedPreferences.getString("selectedSeans", "");
-
-        Toast.makeText(this, "Seçilen Seans: " + selectedSeans, Toast.LENGTH_SHORT).show();
     }
-}
+
+        public void seatClicked(View view){
+            Button seansButton = (Button) view;
+            String selectedSeat = seansButton.getText().toString();
+
+            SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("selectedSeat", selectedSeat);
+            editor.apply();
+
+            Intent intent = new Intent(MovieSeatsActivity.this, SummaryAndConfirmationActivity.class);
+            startActivity(intent);
+        }
+    }
+
