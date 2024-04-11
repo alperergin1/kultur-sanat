957eb0d1e5ccf10eb00f47b1d2173e290650df8f
alpersmb <alper1ergin@gmail.com>
2024-04-11 03:25:59 +0300

    sinema seans seçme sayfasındaki butonlar ile koltuk seçmeye yönlendirdim (henüz adımları kayıt etmeyi yapmadım)

--- app/src/main/java/com/example/kultursanat/CinemaActivity.java
+++ app/src/main/java/com/example/kultursanat/CinemaActivity.java
@@ -13,11 +13,44 @@ public class CinemaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cinema);
 
+        findViewById(R.id.button_film1_seans1e_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
 
-    }
-    /*private void startMovieTimesActivity() {
-        Intent intent = new Intent(CinemaActivity.this, MovieTimesActivity.class);
+        findViewById(R.id.button_film1_seans1e_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
+
+        findViewById(R.id.button_film1_seans2ye_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
+
+        findViewById(R.id.button_film2_seans1e_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
+
+        findViewById(R.id.button_film2_seans2ye_git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMovieSeatsActivity();
+            }
+        });
+     }
+
+    private void startMovieSeatsActivity() {
+        Intent intent = new Intent(CinemaActivity.this, MovieSeatsActivity.class);
         startActivity(intent);
     }
-     */
 }
